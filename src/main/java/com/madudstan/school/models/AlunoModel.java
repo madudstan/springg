1d423ca62bafb26db5a8ecb0e756486e9509b19c
unknown <madudstan@gmail.com>
2026-02-25 19:57:26 -0300

    model criada

---
+++ src/main/java/com/madudstan/school/models/AlunoModel.java
@@ -0,0 +1,50 @@
+package com.madudstan.school.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class AlunoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String endereco;
+    private String email;
+
+    public AlunoModel(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
